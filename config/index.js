ed8bd908a55810e4728997343e2da025106feb70
Lucifer0318 <66894163+Lucifer0318@users.noreply.github.com>
2022-09-21 00:21:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -74,11 +74,12 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "",
+      openUrl: "https://user.qzone.qq.com/1768674705/main",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "宝贝", "year": "1997", "date": "09-01"},
+         {"type": "生日", "name": "小林", "year": "1999", "date": "03-18"},
         {"type": "节日", "name": "恋爱纪念日", "year": "2019", "date": "09-15"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
@@ -90,19 +91,7 @@ export const config = {
         // 退伍日
         // {"keyword": "ex_day", date: "2022-09-10"},
       ]
-    },
-//     {
-//       name: "老婆1",
-//       id: "",
-//       useTemplateId: "",
-//       province: "",
-//       city: "",
-//       horoscopeDate: '',
-//       horoscopeDateType: '',
-//       openUrl: "https://wangxinleo.cn",
-//       festivals: [],
-//       customizedDateList: []
-//     },
+    }
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -120,7 +109,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "RQyaLMhIYEK_R-OFlVLW8lZw40fWF-SSQ7qGBD2fqYE",
+      id: "oJaO45lE7Svg2f9GiwYA21cXZveI",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -180,16 +169,16 @@ export const config = {
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
     * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }}
     * */
-  CUSTOMIZED_DATE_LIST: [
-    // 在一起的日子
-    {"keyword": "love_day", date: "2022-09-08"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2022-09-09"},
-    // 退伍日
-    {"keyword": "ex_day", date: "2022-09-10"},
-    // 你可以不断按格式往下增加
-    // ...
-  ],
+//   CUSTOMIZED_DATE_LIST: [
+//     // 在一起的日子
+//     {"keyword": "love_day", date: "2022-09-08"},
+//     // 结婚纪念日
+//     {"keyword": "marry_day", date: "2022-09-09"},
+//     // 退伍日
+//     {"keyword": "ex_day", date: "2022-09-10"},
+//     // 你可以不断按格式往下增加
+//     // ...
+//   ],
 
   /** 插槽 */
 
