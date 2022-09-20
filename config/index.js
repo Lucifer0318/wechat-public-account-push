22d4fc0c1b40e3c6b41ce7c1150287fa89b71b11
Lucifer0318 <66894163+Lucifer0318@users.noreply.github.com>
2022-09-21 00:52:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oJaO45lE7Svg2f9GiwYA21cXZveI",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "n7lbjEa59z8UiaAURX4tVbef2-j9azKOiMf0L2uCh0Y",
+      useTemplateId: "pE1h7hznwcpJSCrYd9KeBXiDOygbDEl1G57wnbnH94Y",
       // 所在省份
       province: "重庆",
       // 所在城市
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://user.qzone.qq.com/1768674705/main",
+      openUrl: "https://weathernew.pae.baidu.com/weathernew/pc?query=%E9%87%8D%E5%BA%86%E5%A4%A9%E6%B0%94&srcid=4982",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
