ee11c1eefe927a1d443176707237ff1e942cfa6a
Lucifer <66894163+Lucifer0318@users.noreply.github.com>
2022-09-22 11:15:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wxb8a72d8e444f64d2",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "edb8d14923aef77e461f5343fd81862d",
+  APP_SECRET: "",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,11 +60,11 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "源源",
+      name: "",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oJaO45lE7Svg2f9GiwYA21cXZveI",
+      id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "pE1h7hznwcpJSCrYd9KeBXiDOygbDEl1G57wnbnH94Y",
+      useTemplateId: "",
       // 所在省份
       province: "重庆",
       // 所在城市
@@ -78,14 +78,14 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-//         {"type": "生日", "name": "小林", "year": "1999", "date": "03-18"},
-        {"type": "生日", "name": "宝贝", "year": "1997", "date": "09-01"},
-        {"type": "节日", "name": "恋爱纪念日", "year": "2019", "date": "09-15"},
+//         {"type": "生日", "name": "", "year": "", "date": ""},
+        {"type": "生日", "name": "", "year": "", "date": ""},
+        {"type": "节日", "name": "恋爱纪念日", "year": "", "date": ""},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2019-09-15"},
+        {"keyword": "love_day", date: ""},
         // 结婚纪念日
         // {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
@@ -109,7 +109,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oJaO45lE7Svg2f9GiwYA21cXZveI",
+      id: "",
     },
     // 你可以不断按格式往下增加
     // ...
